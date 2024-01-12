b809e38ef56874fdbdf839c6115bbbadf95ddf5c
Saad_Atif <saadatif1234567@gmail.com>
2024-01-13 03:15:08 +0500

    Completed part4's exiercise#4.19. Accidently removed content is also added. Frontend of notes may need updation

--- Exercises/part4/bloglist/controllers/blogs.js
+++ Exercises/part4/bloglist/controllers/blogs.js
@@ -1,24 +1,67 @@
 const blogsRouter = require("express").Router()
 const Blog = require("../models/blog")
+const User = require("../models/user")
+const jwt = require("jsonwebtoken")
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization")
+  console.log(authorization)
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    if (authorization.includes("bearer "))
+      return authorization.replace("bearer ", "")
+    else return authorization.replace("Bearer ", "")
+  }
+  return null
+}
 
 blogsRouter.get("/", async (request, response) => {
   // Blog.find({}).then((blogs) => {
   //   response.json(blogs)
   // })
 
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post("/", async (request, response) => {
-  const blog = new Blog(request.body)
+  const { body } = request
+
+  if (!body.url || !body.title || !body.author) {
+    response.status(400).json({ error: "Url, Title or Author is missing" })
+  }
+
+  if (!body.likes) {
+    body.likes = 0
+  }
+
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  console.log("decodedToken: ", decodedToken)
+
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "Invalid token" })
+  }
+
+  const user = await User.findById(decodedToken.id)
+
+  const blog = new Blog({
+    url: body.url,
+    title: body.title,
+    author: body.author,
+    likes: body.likes,
+    user: user._id,
+  })
+
+  // const blog = new Blog(request.body)
 
   // blog.save().then((result) => {
   //   response.status(201).json(result)
   // })
 
-  const result = await blog.save()
-  response.status(201).json(result)
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  response.status(201).json(savedBlog)
 })
 
 blogsRouter.delete("/:id", async (request, response) => {
---
+++ Exercises/part4/bloglist/controllers/users.js
@@ -0,0 +1,32 @@
+const bcrypt = require("bcrypt")
+const usersRouter = require("express").Router()
+const User = require("../models/user")
+
+usersRouter.get("/", async (request, response) => {
+  // when an HTTP GET request is made to the /api/users route, the user objects would also contain the contents of the user's notes and not just their id. In a relational database, this functionality would be implemented with a join query. When in reality this data is not being saved in the backend db, it's just to present data. We can also specify which field to show from other collection's data object.
+  const users = await User.find({}).populate("blogs", {
+    title: 1,
+    author: 1,
+    url: 1,
+  })
+  response.json(users)
+})
+
+usersRouter.post("/", async (request, response) => {
+  const { username, name, password } = request.body
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  })
+
+  const savedUser = await user.save()
+
+  response.status(201).json(savedUser)
+})
+
+module.exports = usersRouter
